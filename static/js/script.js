aa230c570623fb0c6a394a841cc5d223dc77b471
Boofy360 <joshuacharlesbooth@hotmail.co.uk>
2020-08-12 14:35:58 +0000

    Add review form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $(".dropdown-trigger").dropdown();
     $('.collapsible').collapsible();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 0,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
