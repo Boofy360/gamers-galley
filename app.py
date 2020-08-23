83939894e3151b062dc7092e1af140356208e30b
Boofy360 <joshuacharlesbooth@hotmail.co.uk>
2020-08-23 09:46:06 +0000

    Bind data to the Edit Review form

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def add_review():
 
 @app.route("/edit_review/<review_id>", methods=["GET", "POST"])
 def edit_review(review_id):
-    review = mongo.db.review.find_one({"_id": ObjectId(review_id)})
+    review = mongo.db.reviews.find_one({"_id": ObjectId(review_id)})
     games = mongo.db.games.find().sort("game_title", 1)
     return render_template("edit_review.html", review=review, games=games)
 
